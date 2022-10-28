a6385b6a40decdac35211ebef78b7b383edd6f5b
Maga Cardenas <maga22mushu@gmail.com>
2022-10-28 15:49:38 -0300

    Maga ejercicio 3 resuleto

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,26 +3,34 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array [0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array [array.length -1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 
-function incrementarPorUno(array) {
+function incrementarPorUno(array) { // [2, 8 , 7].lenght === 3
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for( var i=0; i < array.length; i++ ) {  //3
+
+  array[i] = array[i] + 1 
+}
+  return array
 }
 
 
@@ -30,6 +38,15 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+
+
+
+array.push(elemento)
+
+return array
+
+
+
 }
 
 
@@ -38,6 +55,10 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+
+  array.unshift(elemento)
+
+  return array
 }
 
 
@@ -47,6 +68,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+
+   return palabras.join(" ")
 }
 
 
@@ -54,6 +77,17 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+
+  for (var i=0; i<array.length; i ++) {
+    if(array[i] === elemento){
+
+      return true
+    }
+
+      
+  }
+
+  return false
 }
 
 
@@ -61,6 +95,13 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+var numero = 0
+
+for(var i=0; i<numeros.length; i++){
+  numeros = numeros + numeros[i]
+}
+ return numero
+
 }
 
 
